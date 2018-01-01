1a1469b0261a282c3608d2fa0900be79282e2e54
matt1007k <matt1007k@gmail.com>
2018-01-01 17:52:53 -0500

    Login con facebook, usando devise junto con omniauth-facebook

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+ 
+  root 'main#home'
+
+  devise_for :users, controllers: {
+  	omniauth_callbacks: "users/omniauth_callbacks"
+  }
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
