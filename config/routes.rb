aadfbd0dc74c9a031c0090586cd0cd5ffa835ff6
max <max@maxs-iMac.local>
2018-01-03 13:06:53 -0700

    Terminamos el login con facebook

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
- 
+
   root 'main#home'
 
   devise_for :users, controllers: {
   	omniauth_callbacks: "users/omniauth_callbacks"
   }
 
+  post "custom_sign_up", to: "users/omniauth_callbacks#custom_sign_up"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
